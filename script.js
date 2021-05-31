ea72a1871558d2f4172c8f265cd78aeeca7cebc5
Lena Blank <mine.blank@gmail.com>
2021-05-31 20:38:03 +0200

    change quotes dynamically

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // Get Quotes from API
 let apiQuotes = [];
 
@@ -6,6 +12,8 @@ let apiQuotes = [];
 function newQuote() {
     // Get a random quote from array
     const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    quoteText.textContent = quote.text;
+    authorText.textContent = quote.author;
 }
 
 async function getQuotes() {
